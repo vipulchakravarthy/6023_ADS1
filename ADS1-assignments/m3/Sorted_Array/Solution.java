c93a2b823dfb87b227e9e838a3a0726b5e1c18c0
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-26 17:16:07 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS1-assignments/m3/Sorted_Array/Solution.java
+++ ADS1-assignments/m3/Sorted_Array/Solution.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * the class which contains the.
+ * main method two combine
+ *  two arrays
+ */
 class Solution {
+	/** an empty constructor.
+	*/
 	Solution() {
 	}
-	public static int[] integerArray(String str) {
-		if (str.equals("")) return null;
+	/** this method changes the.
+	*string to integer array.
+	*/
+	public static int[] integerArray(final String str) {
 		return Arrays.stream(str.split(","))
 		.mapToInt(Integer::parseInt).toArray();
 	}
-	public static void main(String args[]) {
+	/** the main method is to read the.
+	*input and print the output.
+	*/
+	public static void main(final String args[]) {
 		Scanner scan = new Scanner(System.in);
 		int[] arrayOne = new int[scan.nextInt()];
 		int[] arrayTwo = new int[scan.nextInt()];
@@ -19,7 +31,18 @@ class Solution {
 		int[] result = new int[arrayOne.length + arrayTwo.length];
 		System.arraycopy(arrayOne, 0, result, 0, arrayOne.length);
 		System.arraycopy(arrayTwo, 0, result, arrayOne.length, arrayTwo.length);
-		Arrays.sort(result);
+		// Arrays.sort(result);
+		int temp = 0;
+		for (int i = 0; i < result.length; i++) {
+			for (int j = 1; j < result.length; j++) {
+				if (result[i] > result[j]) {
+					temp = result[i];
+					result[i] = result[j];
+					result[j] = temp;
+				}
+			}
+		}
 		System.out.println(Arrays.toString(result).replace("[", "").replace("]", "").replace(" ", ""));
 		}
 	}
+
