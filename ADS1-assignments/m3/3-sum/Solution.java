376d8b6b712f2f9a84ac7c9e9b25cd632b573f6f
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-26 12:25:17 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS1-assignments/m3/3-sum/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class ThreeSum {
+	ThreeSum() {
+
+	}
+	public int calculate(int[] array) {
+		int count = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				for (int k = j + 1; k < array.length; j++) {
+					if (array[i] + array[j] + array[k] == 0) {
+						count++;
+					}
+				}
+			}
+		}
+	return count;
+}
+}
+final class Solution {
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int length = scan.nextInt();
+		int[] array = new int[length];
+		ThreeSum obj = new ThreeSum();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = scan.nextInt();
+		}
+		System.out.println(obj.calculate(array));
+	}
+}
