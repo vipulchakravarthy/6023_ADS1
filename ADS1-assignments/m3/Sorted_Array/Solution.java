4c83f2d5fe8bef90b0b37fc7f806f5b761dfc0ee
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-26 16:36:20 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS1-assignments/m3/Sorted_Array/Solution.java
+++ ADS1-assignments/m3/Sorted_Array/Solution.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.*;
 class Solution {
 	Solution() {
 	}
@@ -16,9 +15,7 @@ class Solution {
 		if (arrayOne.length != 0) {
 		arrayOne = integerArray(scan.next());
 		}
-		// if (arrayTwo.length != 0) {
 		arrayTwo = integerArray(scan.next());
-		// }
 		int[] result = new int[arrayOne.length + arrayTwo.length];
 		System.arraycopy(arrayOne, 0, result, 0, arrayOne.length);
 		System.arraycopy(arrayTwo, 0, result, arrayOne.length, arrayTwo.length);
