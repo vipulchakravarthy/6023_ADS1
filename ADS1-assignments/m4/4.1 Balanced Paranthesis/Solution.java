eb98fa955942b561c26cf21cc2378e3dd55a5fb6
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-27 17:55:49 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,58 +1,54 @@
 import java.util.Scanner;
-// class Stack {
-// 	private char[] stack;
-// 	public int top;
-// 	Stack(int length) {
-// 		stack = new char[length];
-// 		top = 0;
-// 	}
-// 	public void push(char c) {
-// 		stack[top++] = c;
-// 	}
-// 	public char pop() {
-// 		if (top!=0) {
-// 		return stack[top--];
-// 	} return ' ';
-// }
-// 	public boolean isEmpty() {
-// 		if (top == 0) {
-// 			return true;
-// 		}
-// 		return false;
-// 	}
-// }
+/**
+ *the class for main method and.
+ * perform the balanced paranthesis.
+ */
 class Solution {
-	String response;
-	Solution() {}
-	public static void balParanthesis(String str) {
-		String[] s = str.split("", str.length());
-		String[] stack = new String[str.length()];
-		int size = 0;
-		for (int i = 0; i < s.length; i++) {
-			if (s[i].equals("{") || s[i].equals("[")|| s[i].equals("(")) {
-				stack[size++] = s[i];
-			} else if (size > 0) {
-				if (s[i].equals("}") && stack[size - 1].equals("{")) {
-					size--;
-				 } else if (s[i].equals("]") && stack[size - 1].equals("[")) {
-					size--;
-				 } else if (s[i].equals(")") && stack[size - 1].equals("(")) {
-				 	size--;
-				}
-			}
-		}
-		if (size == 0) {
-			System.out.println("YES");
-		} else {
-			System.out.println("NO");
-		}
-	}
-
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int range = scan.nextInt();
-		for (int i = 0; i < range; i++) {
-			balParanthesis(scan.next());
-		}
-	}
+    /**
+     * the empty constructor.
+     */
+    Solution() {
+    }
+    /**
+     * this method is to.
+     * check whether the string is
+     * with balanced paranthesis or not
+     * @param      str   The string
+     */
+    public static void balParanthesis(final String str) {
+        String[] s = str.split("", str.length());
+        String[] stack = new String[str.length()];
+        int size = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].equals("{") || s[i].equals("[") || s[i].equals("(")) {
+                stack[size++] = s[i];
+            } else if (size > 0) {
+                if (s[i].equals("}") && stack[size - 1].equals("{")) {
+                    size--;
+                 } else if (s[i].equals("]") && stack[size - 1].equals("[")) {
+                    size--;
+                 } else if (s[i].equals(")") && stack[size - 1].equals("(")) {
+                    size--;
+                }
+            }
+        }
+        if (size == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+/**
+ * the main method is.
+ * to read the input.
+ *
+ * @param  args  The arguments
+ */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int range = scan.nextInt();
+        for (int i = 0; i < range; i++) {
+            balParanthesis(scan.next());
+        }
+    }
 }
