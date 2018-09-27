0ef14c99cd2d37945184358920980c845ca0e047
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-27 17:48:16 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,62 +1,53 @@
 import java.util.Scanner;
-class Stack {
-	private char[] stack;
-	public int top;
-	Stack(int length) {
-		stack = new char[length];
-		top = 0;
-	}
-	public void push(char c) {
-		stack[top++] = c;
-	}
-	public char pop() {
-		if (top!=0) {
-		return stack[top--];
-	} return ' ';
-}
-	public boolean isEmpty() {
-		if (top == 0) {
-			return true;
-		}
-		return false;
-	}
-}
+// class Stack {
+// 	private char[] stack;
+// 	public int top;
+// 	Stack(int length) {
+// 		stack = new char[length];
+// 		top = 0;
+// 	}
+// 	public void push(char c) {
+// 		stack[top++] = c;
+// 	}
+// 	public char pop() {
+// 		if (top!=0) {
+// 		return stack[top--];
+// 	} return ' ';
+// }
+// 	public boolean isEmpty() {
+// 		if (top == 0) {
+// 			return true;
+// 		}
+// 		return false;
+// 	}
+// }
 class Solution {
 	String response;
 	Solution() {}
-	public static void balParanthesis(String s) {
-		Stack stackObj = new Stack(s.length());
-		char result = ' ';
-		int flag = 0;
-		for (int i = 0; i < s.length(); i++) {
-			flag = 0;
-			if ((s.charAt(i) == '{') || (s.charAt(i) == '[') || (s.charAt(i) == '(')) {
-				stackObj.push(s.charAt(i));
-			} else {
-				 result = stackObj.pop();
-				 if (result == ' ') {
-				 	flag = 1;
-				 	break;
-				 }
-				 if (result == '{' && s.charAt(i) == '}') {
-				 	continue;
-				 } else if (result == '[' && s.charAt(i) == ']') {
-					continue;
-				 } else if (result == '(' && s.charAt(i) == ')') {
-				 	continue;
-				 } else {
-				 	flag = 1;
-				 	break;
-				 }
+	public static void balParanthesis(String str) {
+		String[] s = str.split("", str.length());
+		String[] stack = new String[str.length()];
+		int size = 0;
+		for (int i = 0; i < s.length; i++) {
+			if (s[i].equals("{") || s[i].equals("[")|| s[i].equals("(")) {
+				stack[size++] = s[i];
+			} else if (size > 0) {
+				if (s[i].equals("}") && stack[size - 1].equals("{")) {
+					size--;
+				 } else if (s[i].equals("]") && stack[size - 1].equals("[")) {
+					size--;
+				 } else if (s[i].equals(")") && stack[size - 1].equals("(")) {
+				 	size--;
+				}
 			}
-			}
-			// System.out.println(stackObj.top + "    " +flag);
-		if (stackObj.top == 1 && flag == 1) {
+		}
+		if (size == 0) {
 			System.out.println("YES");
 		} else {
 			System.out.println("NO");
 		}
 	}
+
 	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int range = scan.nextInt();
