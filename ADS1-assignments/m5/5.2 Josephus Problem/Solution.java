ad21f2884324a2fabcbe3fa1a1b472a2cdecc27f
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-29 17:01:36 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS1-assignments/m5/5.2 Josephus Problem/Solution.java
+++ ADS1-assignments/m5/5.2 Josephus Problem/Solution.java
@@ -1,15 +1,32 @@
 import java.util.Scanner;
+/**
+ *the class for applying circular queue.
+ */
 class CircularQueue {
-	Operations obj;
+	/**
+	 * the object for operations class.
+	 */
+	private Operations obj;
 	CircularQueue() {
 		obj = new Operations();
 	}
-	public void push(int number) {
+	/**
+	 *the method is to push the elements.
+	 *to queue.
+	 * @param      number  The number
+	 */
+	public void push(final int number) {
 		for (int i = 0; i < number; i++) {
 			obj.insertEnd(i);
 		}
 	}
-	public void pop(int number, int length) {
+	/**
+	 * the method to perform circular.
+	 *queue
+	 * @param      number  The number
+	 * @param      length  The length
+	 */
+	public void pop(final int number, final int length) {
 		int[] arr = new int[length];
 		Node temp = obj.head;
 		int range = number;
@@ -22,32 +39,42 @@ class CircularQueue {
 				obj.deleteStart();
 				counter++;
 			}
-		arr[index++] = temp.data;
-		temp = temp.next;
-		counter = 0;
-		obj.deleteStart();
+			arr[index++] = temp.data;
+			temp = temp.next;
+			counter = 0;
+			obj.deleteStart();
 		}
-		// arr[index++] = temp.data;
 		for (int i = 0; i < length - 1; i++) {
 			System.out.print(arr[i] + " ");
 		}
 		System.out.println(arr[length - 1]);
 	}
 }
+/**
+ *the class for main method.
+ */
 class Solution {
+	/**
+	 *an empty constructor.
+	 */
 	Solution() {
 	}
-	public static void main(String[] args) {
+	/**
+	 *the main method to give user input.
+	 *
+	 * @param  args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		CircularQueue obj = new CircularQueue();
 		int lines = scan.nextInt();
 		scan.nextLine();
-		while(scan.hasNext()) {
+		while (scan.hasNext()) {
 			String line = scan.nextLine();
 			String[] tokens = line.split(" ");
 			obj.push(Integer.parseInt(tokens[0]));
 			obj.pop(Integer.parseInt(tokens[1]),
-				Integer.parseInt(tokens[0]));
+			        Integer.parseInt(tokens[0]));
 		}
 	}
 }
