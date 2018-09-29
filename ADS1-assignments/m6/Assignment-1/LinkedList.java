70eff9a646286beaedd6a7b1df023a4d9e69352b
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-09-29 12:10:27 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS1-assignments/m6/Assignment-1/LinkedList.java
+++ ADS1-assignments/m6/Assignment-1/LinkedList.java
@@ -69,7 +69,7 @@ class LinkedList{
 	public int deleteEnd() {
 		int data = tail.data;
 		Node temp = head;
-		while (temp.next.next != null) {
+		while (temp.next != null) {
 			temp = temp.getAddress();
 		}
 		tail = temp;
