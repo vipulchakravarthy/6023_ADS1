3856e43f8555aec0fd033d98cd9a4b78caacb033
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-10-01 17:16:05 +0530

    "check_style_score -> 8 of 12 passed. style score: 0/1 "

--- ADS1-assignments/m6/Assignment-1/Solution.java
+++ ADS1-assignments/m6/Assignment-1/Solution.java
@@ -67,13 +67,13 @@ final class AddLargeNumbers {
         if (list1.size > list2.size) {
             diff = list1.size - list2.size;
             for (int i = 0; i < diff; i++) {
-                list1.insertStart(0);
+                list1.insertEnd(0);
             }
         }
         if (list2.size > list1.size) {
             diff = list2.size - list1.size;
             for (int i = 0; i < diff; i++) {
-                list2.insertStart(0);
+                list2.insertEnd(0);
             }
         }
         if (list1.size == list2.size) {
