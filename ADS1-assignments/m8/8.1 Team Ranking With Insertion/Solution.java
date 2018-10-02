e9ec8b0000349ad9d95f70e6b88f844ededbcde2
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-10-02 16:54:08 +0530

    "check_style_score -> 3 of 6 passed. style score: 0/1 "

--- ADS1-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS1-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -30,13 +30,25 @@ class LeaderBoard {
  	LeaderBoard() {
  		list = new ArrayList<Team>();
 	}
-	public int compareTo(int a, int b) {
-		if (a > b) {
+	public int compareTo(Team one, Team two) {
+		if (one.getWins() < two.getWins()) {
+			return  -1;
+		} else if (one.getWins() > two.getWins()) {
 			return 1;
-		} else if (a < b) {
-			return -1;
+		} else {
+			if (one.getLosses() < two.getLosses()) {
+				return 1;
+			} else if (one.getLosses() > two.getLosses()) {
+				return -1;
+			} else {
+				if (one.getDraw() < two.getDraw()) {
+					return -1;
+				} else if(one.getDraw() < two.getDraw()) {
+					return 1;
+				}
+			}
 		}
-		return 0;
+		return 100;
 	}
 	// public boolean high(int a, int b) {
 	// 	return compareTo(a, b) > 0;
@@ -52,29 +64,15 @@ class LeaderBoard {
 	public void sorting() {
 		int max = 0;
 		int flag = 0;
-		int temp = 0;
-		int tempTwo = 0;
 		for (int i = 0; i < list.size(); i++) {
 			max = i;
 			for (int j = i + 1; j < list.size(); j++) {
-				flag = compareTo(list.get(j).getWins(), list.get(i).getWins());
-				if (flag == 0) {
-					temp = compareTo(list.get(j).getLosses(), list.get(i).getLosses());
-					if (temp > 0) {
-						max = i;
-					} else if(temp == 0) {
-						tempTwo = compareTo(list.get(j).getDraw(), list.get(i).getDraw());
-						if (tempTwo > 0) {
-							max = j;
-						}
-					}
-				} else if (flag > 0) {
+				flag = compareTo(list.get(i), list.get(j));
+				if (flag == -1) {
 					max = j;
-				} else {
-					max = i;
 				}
 			}
-			Collections.swap(list, i, max);
+		Collections.swap(list, i, max);
 		}
 	}
 	public String print() {
