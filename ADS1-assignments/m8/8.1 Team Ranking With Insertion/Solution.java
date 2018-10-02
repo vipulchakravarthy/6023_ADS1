65920ac2eb9955d050e8385674592a10f3e70498
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-10-02 15:45:34 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ ADS1-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+class Team {
+	String teamName;
+	int wins;
+	int losses;
+	int tie;
+	Team(String name, int won, int loss, int draw) {
+		teamName = name;
+		wins = won;
+		losses = loss;
+		tie = draw;
+	}
+	public String getName() {
+		return teamName;
+	}
+	public int getWins() {
+		return wins;
+	}
+	public int getLosses() {
+		return losses;
+	}
+	public int getDraw() {
+		return tie;
+	}
+}
+class LeaderBoard {
+	ArrayList<Team> list;
+ 	LeaderBoard() {
+ 		list = new ArrayList<Team>();
+	}
+	public int compareTo(int a, int b) {
+		if (a > b) {
+			return 1;
+		} else if (a < b) {
+			return -1;
+		}
+		return 0;
+	}
+	public boolean high(int a, int b) {
+		return compareTo(a, b) > 0;
+	}
+	// public void swap(ArrayList<Team> list, int a, int b) {
+	// 	Team temp = list[a];
+	// 	list[a] = list[b];
+	// 	list[b] = temp;
+	// }
+	public void addTo(Team obj) {
+		list.add(obj);
+	}
+	public void sorting() {
+		int max = 0;
+		for (int i = 0; i < list.size(); i++) {
+			max = i;
+			for (int j = i + 1; j < list.size(); j++) {
+				if (high(list.get(j).getWins(), list.get(i).getWins())) {
+					max = j;
+				}
+			}
+			Collections.swap(list, i, max);
+		}
+	}
+	public String print() {
+		String str = "";
+		for (int i = 0; i < list.size() - 1; i++) {
+			str += list.get(i).getName() + ", ";
+		}
+		str += list.get(list.size() - 1);
+		return str;
+	}
+}
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Team obj;
+		LeaderBoard object = new LeaderBoard();
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNext()) {
+			String line = scan.nextLine();
+			String[] tokens = line.split(",");
+			obj = new Team(tokens[0],
+				Integer.parseInt(tokens[1]),
+				Integer.parseInt(tokens[2]),
+				Integer.parseInt(tokens[3]));
+			object.addTo(obj);
+		}
+		object.sorting();
+		System.out.println(object.print());
+	}
+}
