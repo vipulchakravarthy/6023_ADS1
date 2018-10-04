aa23b4a2e6917604f35783c25ce75fdd0112aea7
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-10-04 16:48:14 +0530

    "ads1_m10_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1-assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+class Node{
+	int data;
+	Node next;
+	Node(final int element) {
+		data = element;
+		next = null;
+	}
+	public int getData() {
+		return data;
+	}
+	public Node getNext(){
+		return next;
+	}
+	public void setNext(final Node obj) {
+		next = obj;
+	}
+}
+class LinkedList{
+	Node head;
+	int counter;
+	Node tail;
+	int position;
+	int size;
+	Node temp;
+	LinkedList() {
+		position = 0;
+		size = 0;
+		counter = 1;
+		temp = head;
+	}
+	public void insertAt(int index, int element) throws Exception {
+		if ( index < 0 || index > size) {
+			throw new Exception("Can't insert at this position.");
+		}
+		Node obj = new Node(element);
+		if (size == 0){
+			head = obj;
+			temp = head;
+			size++;
+			return;
+		}
+		if (index == 0) {
+			obj.setNext(head);
+			head = obj;
+			size++;
+			return;
+		} else if(index == counter) {
+			obj.setNext(temp.getNext());
+			temp.setNext(obj);
+			temp = head;
+			counter = 1;
+			size++;
+			return;
+		}
+		temp = temp.getNext();
+		counter++;
+		insertAt(index , element);
+	}
+	public void reverse() {
+
+	}
+	public String print() {
+		String str = "";
+		Node temp = head;
+		while (temp.getNext() != null) {
+			str += temp.getData() + ", ";
+			temp = temp.getNext();
+		}
+		str += temp.getData();
+		return str;
+	}
+}
+class Solution{
+	Solution() {}
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		LinkedList object = new LinkedList();
+		while (scan.hasNext()) {
+			String line = scan.nextLine();
+			String[] tokens = line.split(" ");
+			switch (tokens[0]) {
+				case "insertAt":
+				try {
+					object.insertAt(Integer.parseInt(tokens[1]),
+						Integer.parseInt(tokens[2]));
+					System.out.println(object.print());
+				} catch(Exception e) {
+						System.out.println(e);
+				}
+				break;
+				case "reverse":
+					// object.reverse();
+					break;
+				default: break;
+			}
+		}
+	}
+}
