61d5953700f3b78f9439b20f18e032bbac5a22ac
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-10-09 20:48:00 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ADS1-assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -1,39 +1,103 @@
-import java.util.Arrays;
 import java.util.Scanner;
+/**
+ *the class is for performing dynamic median.
+ */
 class DynamicMedian{
-	MinPQ<Double> minObj = new MinPQ<Double>();
-	MaxPQ<Double> maxObj = new MaxPQ<Double>();
+	/**
+	 *the object to access of minheap.
+	 */
+	private MinPQ<Double> minObj;
+	/**
+	 *the object to access of maxheap.
+	 */
+	private MaxPQ<Double> maxObj;
+	/**
+	 *the constructor for initializing.
+	 */
 	DynamicMedian(){
+		minObj = new MinPQ<Double>();
+		maxObj = new MaxPQ<Double>();
 	}
+	/**
+	 *the method will insert the element
+	 *to minheap.
+	 * @param      item  The item
+	 */
 	public void insertAtMin(double item){
 		minObj.insert(item);
 	}
+	/**
+	 *the method will insert the element
+	 *to maxheap.
+	 * @param      item  The item
+	 */
 	public void insertAtMax(double item){
 		maxObj.insert(item);
 	}
+	/**
+	 *this method gets the minimum element.
+	 * from minheap.
+	 * @return     The minimum.
+	 */
 	public double getMin(){
 	return minObj.min();
 	}
+	/**
+	 *this method gets the maximum element.
+	 * from maxheap.
+	 * @return     The minimum.
+	 */
 	public double getMax() {
 		return maxObj.max();
 	}
+	/**
+	 *this method delete the maximum element.
+	 * from maxheap.
+	 * @return     The minimum.
+	 */
 	public double delMax(){
 		return maxObj.delMax();
 	}
+	/**
+	 *this method delete the minimum element.
+	 * from maxheap.
+	 * @return     The minimum.
+	 */
 	public double delMin(){
 		return minObj.delMin();
 	}
+	/**
+	 *gets the size of minheap.
+	 *
+	 * @return     The minimum size.
+	 */
 	public int getMinSize(){
 		return minObj.size();
 	}
+	/**
+	 * Gets the maximum size of maxheap.
+	 *
+	 * @return     The maximum size.
+	 */
 	public int getMaxSize(){
 		return maxObj.size();
 	}
 }
-class Solution{
-	Solution() {
+/**
+ *this class is for main method.
+ */
+final class Solution{
+	/**
+	 *an empty constructor.
+	 */
+	private Solution() {
 	}
-	public static void main(String[] args) {
+	/**
+	 *the main method is to.
+	 *take the input from user.
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		DynamicMedian obj = new DynamicMedian();
 		long inputs = scan.nextLong();
@@ -56,7 +120,8 @@ class Solution{
 			if ( obj.getMaxSize()- obj.getMinSize()  > 1) {
 				obj.insertAtMin(obj.delMax());
 			}
-			if (Math.abs(obj.getMinSize() - obj.getMaxSize()) == 1) {
+			if (Math.abs(
+				obj.getMinSize() - obj.getMaxSize()) == 1) {
 				if ( obj.getMinSize() > obj.getMaxSize()) {
 					median = obj.getMin();
 					System.out.println(median);
