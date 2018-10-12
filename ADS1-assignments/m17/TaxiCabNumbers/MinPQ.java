8f8c3bf0943ecd5f02931e7405f8a7ed4dd29adb
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-10-12 15:40:26 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignments/m17/TaxiCabNumbers/MinPQ.java
+++ ADS1-assignments/m17/TaxiCabNumbers/MinPQ.java
@@ -16,7 +16,6 @@
  *  (ala insertion sort).
  *
  ******************************************************************************/
-
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -98,8 +97,8 @@ public class MinPQ<Key> implements Iterable<Key> {
         n = keys.length;
         pq = (Key[]) new Object[keys.length + 1];
         for (int i = 0; i < n; i++)
-            pq[i + 1] = keys[i];
-        for (int k = n / 2; k >= 1; k--)
+            pq[i+1] = keys[i];
+        for (int k = n/2; k >= 1; k--)
             sink(k);
         assert isMinHeap();
     }
@@ -170,41 +169,42 @@ public class MinPQ<Key> implements Iterable<Key> {
         Key min = pq[1];
         exch(1, n--);
         sink(1);
-        pq[n + 1] = null;   // to avoid loiterig and help with garbage collection
+        pq[n+1] = null;     // to avoid loiterig and help with garbage collection
         if ((n > 0) && (n == (pq.length - 1) / 4)) resize(pq.length / 2);
         assert isMinHeap();
         return min;
     }
 
 
-    /***************************************************************************
-     * Helper functions to restore the heap invariant.
-     ***************************************************************************/
+   /***************************************************************************
+    * Helper functions to restore the heap invariant.
+    ***************************************************************************/
 
     private void swim(int k) {
-        while (k > 1 && greater(k / 2, k)) {
-            exch(k, k / 2);
-            k = k / 2;
+        while (k > 1 && greater(k/2, k)) {
+            exch(k, k/2);
+            k = k/2;
         }
     }
 
     private void sink(int k) {
-        while (2 * k <= n) {
-            int j = 2 * k;
-            if (j < n && greater(j, j + 1)) j++;
+        while (2*k <= n) {
+            int j = 2*k;
+            if (j < n && greater(j, j+1)) j++;
             if (!greater(k, j)) break;
             exch(k, j);
             k = j;
         }
     }
 
-    /***************************************************************************
-     * Helper functions for compares and swaps.
-     ***************************************************************************/
+   /***************************************************************************
+    * Helper functions for compares and swaps.
+    ***************************************************************************/
     private boolean greater(int i, int j) {
         if (comparator == null) {
             return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
-        } else {
+        }
+        else {
             return comparator.compare(pq[i], pq[j]) > 0;
         }
     }
@@ -223,8 +223,8 @@ public class MinPQ<Key> implements Iterable<Key> {
     // is subtree of pq[1..n] rooted at k a min heap?
     private boolean isMinHeap(int k) {
         if (k > n) return true;
-        int left = 2 * k;
-        int right = 2 * k + 1;
+        int left = 2*k;
+        int right = 2*k + 1;
         if (left  <= n && greater(k, left))  return false;
         if (right <= n && greater(k, right)) return false;
         return isMinHeap(left) && isMinHeap(right);
@@ -265,20 +265,20 @@ public class MinPQ<Key> implements Iterable<Key> {
         }
     }
 
-    // /**
-    //  * Unit tests the {@code MinPQ} data type.
-    //  *
-    //  * @param args the command-line arguments
-    //  */
-    // public static void main(String[] args) {
-    //     MinPQ<String> pq = new MinPQ<String>();
-    //     while (!StdIn.isEmpty()) {
-    //         String item = StdIn.readString();
-    //         if (!item.equals("-")) pq.insert(item);
-    //         else if (!pq.isEmpty()) StdOut.print(pq.delMin() + " ");
-    //     }
-    //     StdOut.println("(" + pq.size() + " left on pq)");
-    // }
+    /**
+     * Unit tests the {@code MinPQ} data type.
+     *
+     * @param args the command-line arguments
+     */
+//     public static void main(String[] args) {
+//         MinPQ<String> pq = new MinPQ<String>();
+//         while (!StdIn.isEmpty()) {
+//             String item = StdIn.readString();
+//             if (!item.equals("-")) pq.insert(item);
+//             else if (!pq.isEmpty()) StdOut.print(pq.delMin() + " ");
+//         }
+//         StdOut.println("(" + pq.size() + " left on pq)");
+//     }
 
 }
 
