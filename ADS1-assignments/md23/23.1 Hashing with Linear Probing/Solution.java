e02e00975cc18c93db9cfd9ce0521524d09b47ce
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-10-26 17:59:55 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignments/md23/23.1 Hashing with Linear Probing/Solution.java
+++ ADS1-assignments/md23/23.1 Hashing with Linear Probing/Solution.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.io.File;
-class LinearProbing<Key, Value>{
+class LinearProbing<Key, Value> {
 	/**
 	 *the variable for initial capacity.
 	 */
@@ -25,7 +25,7 @@ class LinearProbing<Key, Value>{
 	 *constructor to initialize the array.
 	 *length.
 	 */
-	public LinearProbing(){
+	public LinearProbing() {
 		this(capacity);
 	}
 	/**
@@ -33,7 +33,7 @@ class LinearProbing<Key, Value>{
 	 * keys array and value array
 	 * @param      capacity  The capacity
 	 */
-	LinearProbing(final int capacity){
+	LinearProbing(final int capacity) {
 		length = capacity;
 		keyValuePairs = 0;
 		keys = (Key[]) new Object[length];
@@ -45,7 +45,7 @@ class LinearProbing<Key, Value>{
 	 *
 	 * @return hash value of particular key.
 	 */
-	private int hash(final Key key){
+	private int hash(final Key key) {
 		return ((11 * key.hashCode()) % length);
 	}
 	/**
@@ -56,7 +56,7 @@ class LinearProbing<Key, Value>{
 	 * @return  whether the key is present
 	 * or not.
 	 */
-	public boolean contains(final Key key){
+	public boolean contains(final Key key) {
 		return get(key) != null;
 	}
 	/**
@@ -67,17 +67,17 @@ class LinearProbing<Key, Value>{
 	 *the size of array.
 	 * @param     capacity  The capacity
 	 */
-	public void resize(final int capacity){
-	LinearProbing<Key, Value> temp
-	= new LinearProbing<Key, Value>(capacity);
-	for(int i = 0; i < length; i++){
-		if(keys[i] != null){
-			temp.put(keys[i], values[i]);
+	public void resize(final int capacity) {
+		LinearProbing<Key, Value> temp
+		    = new LinearProbing<Key, Value>(capacity);
+		for (int i = 0; i < length; i++) {
+			if (keys[i] != null) {
+				temp.put(keys[i], values[i]);
+			}
 		}
-	}
-	keys = temp.keys;
-	values = temp.values;
-	length = temp.length;
+		keys = temp.keys;
+		values = temp.values;
+		length = temp.length;
 	}
 	/**
 	 *this method is to put the
@@ -87,16 +87,16 @@ class LinearProbing<Key, Value>{
 	 * @param   key    The key
 	 * @param   value  The value
 	 */
-	public void put(final Key key, final Value value){
-		if(keyValuePairs >= (length / 2)) {
+	public void put(final Key key, final Value value) {
+		if (keyValuePairs >= (length / 2)) {
 			resize(2 * length);
 		}
 		int i;
-		for(i = hash(key); keys[i] != null; i = (i + 1) % length){
-			if(keys[i].equals(key)){
+		for (i = hash(key); keys[i] != null; i = (i + 1) % length) {
+			if (keys[i].equals(key)) {
 				values[i] = value;
 				return;
-		}
+			}
 		}
 		keys[i] = key;
 		values[i] = value;
@@ -110,10 +110,10 @@ class LinearProbing<Key, Value>{
 	 * @return it returns the value of
 	 * particular key.
 	 */
-	public Value get(final Key key){
+	public Value get(final Key key) {
 		int i;
-		for(i = hash(key); keys[i] != null; i = (i + 1) % length){
-			if(keys[i].equals(key)){
+		for (i = hash(key); keys[i] != null; i = (i + 1) % length) {
+			if (keys[i].equals(key)) {
 				return values[i];
 			}
 		}
@@ -125,15 +125,15 @@ class LinearProbing<Key, Value>{
 	 *time complexity is O(N)
 	 * @param      key   The key
 	 */
-	public void delete(final Key key){
+	public void delete(final Key key) {
 		if (!contains(key)) return;
 		int i;
-		for(i = hash(key); !keys[i].equals(key); i = (i + 1) % length){
+		for (i = hash(key); !keys[i].equals(key); i = (i + 1) % length) {
 		}
 		keys[i] = null;
 		values[i] = null;
 		i = (i + 1) % length;
-		while(keys[i] != null){
+		while (keys[i] != null) {
 			Key tempKey = keys[i];
 			Value tempValue = values[i];
 			keyValuePairs--;
@@ -143,7 +143,7 @@ class LinearProbing<Key, Value>{
 			i = (i + 1) % length;
 		}
 		keyValuePairs--;
-		if(keyValuePairs >= 0 && keyValuePairs <= length / 8){
+		if (keyValuePairs >= 0 && keyValuePairs <= length / 8) {
 			resize(length / 2);
 		}
 	}
@@ -153,25 +153,25 @@ class LinearProbing<Key, Value>{
 	 * @return  queue which is filled with key-value.
 	 */
 	public Iterable<Key> keys() {
-        Queue<Key> queue = new Queue<Key>();
-        for (int i = 0; i < length; i++) {
-            if (keys[i] != null) {
-            	queue.enqueue(keys[i]);
-        	}
-    	}
-        return queue;
-    }
-    /**
-     *this method is to give all key value pairs.
-     *time complexity is O(N)
-     */
-	public void display(){
+		Queue<Key> queue = new Queue<Key>();
+		for (int i = 0; i < length; i++) {
+			if (keys[i] != null) {
+				queue.enqueue(keys[i]);
+			}
+		}
+		return queue;
+	}
+	/**
+	 *this method is to give all key value pairs.
+	 *time complexity is O(N)
+	 */
+	public void display() {
 		String str = "{";
-		if(keyValuePairs == 0){
+		if (keyValuePairs == 0) {
 			System.out.println("{}");
 			return;
 		}
-		for(Key key : keys()) {
+		for (Key key : keys()) {
 			str += key + ":" + get(key) + ", ";
 		}
 		String strOne = str.substring(0, str.length() - 2) + "}";
@@ -181,11 +181,11 @@ class LinearProbing<Key, Value>{
 /**
  *the class is for main method.
  */
-final class Solution{
+final class Solution {
 	/**
 	 *an empty constructor.
 	 */
-	private Solution(){
+	private Solution() {
 	}
 	/**
 	 *the main method to take input.
@@ -195,23 +195,23 @@ final class Solution{
 	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		LinearProbing<String, Integer> object
-		= new LinearProbing<String, Integer>();
-		while(scan.hasNext()){
+		    = new LinearProbing<String, Integer>();
+		while (scan.hasNext()) {
 			String[] tokens = scan.nextLine().split(" ");
-			switch(tokens[0]) {
-				case "put":
-					object.put(tokens[1], Integer.parseInt(tokens[2]));
-					break;
-				case "get":
-					System.out.println(object.get(tokens[1]));
-					break;
-				case "delete":
-					object.delete(tokens[1]);
-					break;
-				case "display":
-					object.display();
-					break;
-				default: break;
+			switch (tokens[0]) {
+			case "put":
+				object.put(tokens[1], Integer.parseInt(tokens[2]));
+				break;
+			case "get":
+				System.out.println(object.get(tokens[1]));
+				break;
+			case "delete":
+				object.delete(tokens[1]);
+				break;
+			case "display":
+				object.display();
+				break;
+			default: break;
 			}
 		}
 	}
