ba4edce0770b29b6819fc98bfbca71af177a6918
vipulcharkravarthy <saivipul15@msitprogram.net>
2018-10-27 10:52:09 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1-assignments/md24/1/Solution.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Student{
+	private String rollNumber;
+	private String name;
+	private Double marks;
+	Student(String roll, String studentName, String total){
+		rollNumber = roll;
+		name = studentName;
+		marks = Double.parseDouble(total);
+	}
+	public String getRoll(){
+		return this.rollNumber;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public Double getTotal(){
+		return this.marks;
+	}
+}
+class DataBase{
+	LinearProbingHashST<String, Student> hashObj;
+	DataBase(){
+		hashObj = new LinearProbingHashST<String, Student>();
+	}
+	public void addTo(Student studentObj){
+		hashObj.put(studentObj.getRoll(), studentObj);
+	}
+	public String getStudent(String rollNumber){
+		Student student = hashObj.get(rollNumber);
+		return student.getName();
+	}
+	public Double getTotalMarks(String rollNumber){
+		Student student = hashObj.get(rollNumber);
+		return student.getTotal();
+	}
+
+}
+class Solution{
+	Solution(){
+	}
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		DataBase dataObj = new DataBase();
+		Student studentDetails;
+		int students = scan.nextInt();
+		scan.nextLine();
+		for(int i = 0; i < students; i++){
+			String[] tokens = scan.nextLine().split(",");
+			studentDetails = new Student(tokens[0], tokens[1], tokens[2]);
+			dataObj.addTo(studentDetails);
+		}
+		int queries = scan.nextInt();
+		scan.nextLine();
+		while(queries > 0){
+			String[] check = scan.nextLine().split(" ");
+			switch(check[2]){
+				case "1":
+					System.out.println(dataObj.getStudent(check[1]));
+					break;
+				case "2":
+					System.out.println(dataObj.getTotalMarks(check[1]));
+					break;
+				default:break;
+			}
+			queries--;
+		}
+	}
+}
